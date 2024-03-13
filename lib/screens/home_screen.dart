93df00c75b98127c15d4eef305306a24a58f1727
mahmoud-alwakeel <mahmoudalwakeel200@gmail.com>
2024-03-13 21:21:41 +0200

    show case

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,19 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:show_case/screens/settings_Screen.dart';
 import 'package:show_case/widgets/cups_goal.dart';
 import 'package:show_case/widgets/cups_number_display.dart';
+import 'package:showcaseview/showcaseview.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final key1 = GlobalKey();
+  final key2 = GlobalKey();
+  final key3 = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => ShowCaseWidget.of(context).startShowCase(
+        [
+          key1,
+          key2,
+          key3,
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.indigo,
         leading: IconButton(
-          onPressed: () {},
+          color: Colors.white,
+          onPressed: () {
+            setState(() {
+              ShowCaseWidget.of(context).startShowCase([
+                key1,
+                key2,
+                key3,
+              ]);
+            });
+          },
           icon: const Icon(Icons.help),
         ),
-        title: const Text('Water tracker'),
+        title: const Text(
+          'Water tracker',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
         actions: [
           IconButton(
             onPressed: () {
@@ -25,23 +63,65 @@ class HomeScreen extends StatelessWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.settings),
+            icon: Showcase(
+                key: key1,
+                description: 'anything for now',
+                targetShapeBorder: const CircleBorder(),
+                tooltipBackgroundColor: Colors.indigo,
+                //descTextStyle: TextStyle(color: Colors.white),
+                textColor: Colors.white,
+                targetPadding: const EdgeInsets.all(8),
+                tooltipPadding: const EdgeInsets.all(20),
+                child: const Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                )),
           ),
         ],
       ),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CupsNumberDisplay(),
-          SizedBox(height: 20,),
-          CupsGoal(),
-        ],
+      body: Center(
+        child: Showcase(
+          key: key2,
+          title: 'Title for now',
+          description: 'anything for now',
+          tooltipBackgroundColor: Colors.indigo,
+          //descTextStyle: TextStyle(color: Colors.white),
+          textColor: Colors.white,
+          showArrow: false,
+          targetPadding: const EdgeInsets.all(8),
+          tooltipPadding: const EdgeInsets.all(20),
+          child: const Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupsNumberDisplay(),
+              SizedBox(
+                height: 20,
+              ),
+              CupsGoal(),
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.indigo,
-        shape: const CircleBorder(),
-        child: const Icon(Icons.add, color: Colors.white,),
+      floatingActionButton: Showcase.withWidget(
+        key: key3,
+        width: 50,
+        height: 50,
+        targetShapeBorder: const CircleBorder(),
+        targetPadding: const EdgeInsets.all(8),
+        container: Icon(
+          Icons.local_drink, 
+          size: 50,
+          color: Colors.indigo[400],),
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.indigo,
+          shape: const CircleBorder(),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,28 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:show_case/widgets/cups_number_display.dart';
+import 'package:showcaseview/showcaseview.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  final key4 = GlobalKey();
+  BuildContext? myContext;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      return ShowCaseWidget.of(myContext!).startShowCase(
+        [
+          key4,
+        ],
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Water tracker'),
-      ),
-      body: const Center(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Icon(Icons.remove),
-                CupsNumberDisplay(),
-                Icon(Icons.add),
-              ],
+    return ShowCaseWidget(
+      builder: Builder(
+        builder: (context) {
+          myContext = context;
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Water tracker'),
             ),
-            Text("Adjust you water intake goal"),
-          ],
-        ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Showcase(
+                    key: key4,
+                    title: "change water intake goal",
+                    description: "increase or decrease",
+                     tooltipBackgroundColor: Colors.indigo,
+                     textColor: Colors.white,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(child: Icon(Icons.remove)),
+                        Expanded(child: CupsNumberDisplay()),
+                        Expanded(child: Icon(Icons.add)),
+                      ],
+                    ),
+                  ),
+                  Text("Adjust you water intake goal"),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
